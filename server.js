5f9adaa571e2b985be50cdf9da8eb89e6d4948e1
Walter Moleterno <135560995+Boilermaker74@users.noreply.github.com>
2023-11-11 19:07:21 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const app = express();
 const api = require('./routes/index.js');
 const PORT = process.env.PORT || 3001;
 
-
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
